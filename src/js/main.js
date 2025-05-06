abe24ecf76d46a0f4cd7c94c5a866e0afaaddf78
Annawalla1 <annawallawalla1@gmail.com>
2025-05-06 11:42:22 -0600

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
@@ -11,3 +12,89 @@ function parkInfoTemplate(info) {
       <span>${info.states}</span>
     </p>`;
   }
+  function setHeaderInfo(data) {
+    // insert data into disclaimer section
+    const disclaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = data.url;
+    disclaimer.innerHTML = data.fullName;
+    // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+    document.querySelector("head > title").textContent = data.fullName;
+    // set the banner image
+    document.querySelector(".hero-banner > img").src = data.images[0].url;
+    // use the template function above to set the rest of the park specific info in the header
+    document.querySelector(".hero-banner__content").innerHTML =
+      parkInfoTemplate(data);
+  }
+  const parkInfoLinks = [
+    {
+      name: "Current Conditions &#x203A;",
+      link: "conditions.html",
+      image: parkData.images[2].url,
+      description:
+        "See what conditions to expect in the park before leaving on your trip!"
+    },
+    {
+      name: "Fees and Passes &#x203A;",
+      link: "fees.html",
+      image: parkData.images[3].url,
+      description: "Learn about the fees and passes that are available."
+    },
+    {
+      name: "Visitor Centers &#x203A;",
+      link: "visitor_centers.html",
+      image: parkData.images[9].url,
+      description: "Learn about the visitor centers in the park."
+    }
+  ];
+  function getMailingAddress(addresses) {
+    const mailing = addresses.find((address) => address.type === "Mailing");
+    return mailing;
+  }
+  function footerTemplate(info) {
+    const mailing = getMailingAddress(info.addresses);
+    const voice = getVoicePhone(info.contacts.phoneNumbers)
+    
+    return `<section class="contact">
+    <h3>Contact Info</h3>
+    <h4>Mailing Address:</h4>
+    <div><p>${mailing.line1}<p>
+    <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p></div>
+    <h4>Phone:</h4>
+    <p>${voice}</p>
+    </section>`;
+  }
+  function setHeaderInfo(data) {
+    // insert data into disclaimer section
+    const disclaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = data.url;
+    disclaimer.innerHTML = data.fullName;
+    // update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+    document.querySelector("head > title").textContent = data.fullName;
+    // set the banner image
+    document.querySelector(".hero-banner > img").src = data.images[0].url;
+    // use the template function above to set the rest of the park specific info in the header
+    document.querySelector(".hero-banner__content").innerHTML =
+      parkInfoTemplate(data);
+  }
+  
+  function setParkIntro(data) {
+    const introEl = document.querySelector(".intro");
+    introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+    <p>${parkData.description}</p>`;
+  }
+  
+  function setParkInfoLinks(data) {
+    const infoEl = document.querySelector(".info");
+    const html = data.map(mediaCardTemplate);
+    infoEl.innerHTML = html.join("");
+  }
+  
+  function setFooter(data) {
+    const footerEl = document.querySelector("#park-footer");
+    footerEl.innerHTML = footerTemplate(data);
+  }
+  
+  setHeaderInfo(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(parkInfoLinks);
+  setFooter(parkData);
